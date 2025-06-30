4b33a3199dfeb0d50a7cf87ab28504bc41253de7
Bright Ndoma Obaji <brightobaji8@gmail.com>
2025-06-30 11:06:13 +0100

    Javascript practices added

---
+++ JAVASCRIPT 101/script.js
@@ -0,0 +1,54 @@
+// console.log("page successfully loaded")
+
+
+
+
+// STORING DATA WITH VARIABLES
+// STRING
+// var website=("https://google.com");
+// var teacherName=("Bright Ndoma Obaji");
+
+// console.log (teacherName);
+
+// console.log (website)
+
+// VARIABLES let and const
+
+// var militaryRank= "General";
+// console.log(militaryRank)
+
+
+//  TOPIC Let and const type
+// let apples = 2;
+// const height = "7'0\"";
+//  teacherName= "Bri";
+// console.log(teacherName)
+// var teacherName = "Bright";
+
+// if (teacherName == "Bright") {
+//     let apples =5;
+//     var height = "9'1\"";
+//     console.log("i have", apples, "apples")
+// }
+// console.log ("i am", height);
+
+
+
+
+// const height= "6.0\"";
+
+
+// const name = "bright ndoma";
+// let profession = "Programmer";
+
+
+
+
+const name = "Success";
+if (name == "Bright") {
+    console.log("Welcome Mr Bright");   
+} else if (name == "PETER OBI") {
+    console.log("Welcome Mr Peter");
+} else{
+    console.log ("Welcome guest")
+}
