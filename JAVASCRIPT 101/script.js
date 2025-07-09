879e33fede8814ad3dda518d7519671b6afbae1d
Bright Ndoma Obaji <brightobaji8@gmail.com>
2025-07-09 07:46:09 +0100

    more of javascript

--- JAVASCRIPT 101/script.js
+++ JAVASCRIPT 101/script.js
@@ -72,3 +72,79 @@
 
 // course.search("coding")
 
+
+
+// Demystifying Data Types
+// const boolean =true;
+// const number = 100;
+// const string = "Hello MR BRIGHT";
+// const array = [1,3,4,6,7]
+// const object = {
+//     "name": "Bright", "age": 30, "profession": "programmer", "hobby" :"coding",
+//     "vallage": "Ikom LGA", "staet": "Cross River State"
+// }
+// const nullValue= null;
+// const underdefineValue = underfined;
+// const symbolValue = Symbol ("unique")
+
+
+// TOPIC: CASTING DATA TYPES
+
+// String to Number
+// const strNumber = "123";
+// const num = Number(strNumber); // 123
+
+// // Number to String
+// const numValue = 456;
+// const str = String(numValue); // "456"
+
+// // Boolean to String
+// const boolValue = true;
+// const boolStr = String(boolValue); // "true"
+
+// // String to Boolean
+// const strBool = "false";
+// const boolFromStr = Boolean(strBool); // true (non-empty string is true)
+
+// // Number to Boolean
+// const zero = 0;
+// const one = 1;
+// const boolZero = Boolean(zero); // false
+// const boolOne = Boolean(one); // true
+
+// // Parsing integers and floats
+// const floatStr = "3.14159";
+// const intStr = "42";
+// const parsedFloat = parseFloat(floatStr); // 3.14159
+// const parsedInt = parseInt(intStr); // 42
+
+// console.log(num, str, boolStr, boolFromStr, boolZero, boolOne, parsedFloat, parsedInt);
+
+
+
+// String Concatenation
+
+// const name = "Bright"; age =n30;
+// `my name is $ {name}. and my age is ${age}`
+
+// Javascript Numbers
+// lst = ["one", 2, 3, 4, "five", [], {}]
+// lst.push("new items in here")
+
+// // To pop things out of an array into a variable
+// const newItem = lst.pop()
+
+// Selecting Multiple Nodes
+// const elems = document.querySelectorAll("li");
+// console.log(elems)
+
+
+
+
+// DO NOT REAPET YOURSELF(DRY)
+
+function addNumbers (num1 , num2) {
+    const total = num1 + num2;
+    return total;
+}
+const total = addNumbers(1, 9)
